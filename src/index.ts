53064a27eeb6ebcad69b636b40e43691243d9509
Otavio Lemos <otaviolemos@gmail.com>
2023-09-04 14:53:18 -0300

    feat: add some tests

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,37 @@ import { Bike } from "./bike";
 import { Rent } from "./rent";
 import { User } from "./user";
 
-const bike = new Bike('mountain bike', 'mountain', 
-    123, 500, 100.5, 'desc', 5, [])
-const user = new User('Maria', 'maria@mail.com', '1234')
+const app = new App()
+const user1 = new User('Jose', 'jose@mail.com', '1234')
+const user2 = new User('Maria', 'maria@mail.com', '1234')
+const user1Id = app.registerUser(user1)
+const user2Id = app.registerUser(user2)
+console.log(app.users)
+
+const bike = new Bike('caloi mountain', 'mountain bike', 123, 200, 100.5, 'My bike', 5, [])
+const bikeId = app.registerBike(bike)
+console.log(app.bikes)
+
 const today = new Date()
-const twoDaysFromToday = new Date()
-twoDaysFromToday.setDate(twoDaysFromToday.getDate() + 2)
+const yesterday = new Date()
+yesterday.setDate(yesterday.getDate() - 1)
 const tomorrow = new Date()
 tomorrow.setDate(tomorrow.getDate() + 1)
-const sevenDaysFromToday = new Date()
-sevenDaysFromToday.setDate(sevenDaysFromToday.getDate() + 7)
-const rent1 = Rent.create([], bike, user, today, twoDaysFromToday)
-const user2 = new User('Maria Clara', 'maria@mail.com', '3123')
+const dayAfterTomorrow = new Date()
+dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2)
+const threeDaysFromToday = new Date()
+threeDaysFromToday.setDate(threeDaysFromToday.getDate() + 3)
+const fourDaysFromToday = new Date()
+fourDaysFromToday.setDate(fourDaysFromToday.getDate() + 4)
 
-const app = new App()
-app.registerUser(user)
+app.rentBike(bikeId, 'jose@mail.com', yesterday, today)
 
-console.log(app.findUser('maria@mail.com'))
+console.log('Before return', app.rents)
+
+app.returnBike(bikeId, 'jose@mail.com')
+
+console.log('After return', app.rents)
+
+
+
 
