77670941bdb145a5ebb5b146c10f814969d8c9d3
Otavio Lemos <otaviolemos@gmail.com>
2023-09-04 14:53:02 -0300

    feature: register user, rent, return bike, etc

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,58 @@ export class App {
         return this.users.find(user => user.email === email)
     }
 
-    registerUser(user: User): void {
+    registerUser(user: User): string {
         for (const rUser of this.users) {
             if (rUser.email === user.email) {
                 throw new Error('Duplicate user.')
             }
         }
-        user.id = crypto.randomUUID()
+        const newId = crypto.randomUUID()
+        user.id = newId
         this.users.push(user)
+        return newId
+    }
+
+    registerBike(bike: Bike): string {
+        const newId = crypto.randomUUID()
+        bike.id = newId
+        this.bikes.push(bike)
+        return newId
+    }
+
+    removeUser(email: string): void {
+        const userIndex = this.users.findIndex(user => user.email === email)
+        if (userIndex !== -1) {
+            this.users.splice(userIndex, 1)
+            return
+        }
+        throw new Error('User not found.')
+    }
+
+    rentBike(bikeId: string, userEmail: string, startDate: Date, endDate: Date) {
+        const bike = this.bikes.find(bike => bike.id === bikeId)
+        if (!bike) {
+            throw new Error('Bike not found.')
+        }
+        const user = this.users.find(user => user.email === userEmail)
+        if (!user) {
+            throw new Error('User not found.')
+        }
+        const bikeRents = 
+            this.rents.filter(rent => rent.bike.id === bikeId && !rent.dateReturned)
+        const newRent = Rent.create(bikeRents, bike, user, startDate, endDate)
+        this.rents.push(newRent)
+    }
+
+    returnBike(bikeId: string, userEmail: string): void {
+        const today = new Date()
+        const rent = this.rents.find(rent => 
+            rent.bike.id === bikeId && 
+            rent.user.email === userEmail &&
+            rent.dateFrom < today
+        )
+        if (rent) {
+            rent.dateReturned = today
+        }
     }
 }
