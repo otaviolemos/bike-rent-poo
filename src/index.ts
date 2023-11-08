82139a4057f87cf601a85409b60ef9dc195cd774
Otavio Lemos <otaviolemos@gmail.com>
2023-11-08 20:50:44 -0300

    feat: updated index

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,30 @@ import { PrismaUserRepo } from "./external/database/prisma-user-repo";
 import { Rent } from "./rent";
 import { User } from "./user";
 import sinon from 'sinon'
+import prisma from "./external/database/db"
 
 async function main() {
+    await prisma.user.deleteMany({})
+    await prisma.bike.deleteMany({})
+    await prisma.rent.deleteMany({})
     const clock = sinon.useFakeTimers();
     const userRepo = new PrismaUserRepo()
     const bikeRepo = new PrismaBikeRepo()
     const rentRepo = new PrismaRentRepo()
     const app = new App(userRepo, bikeRepo, rentRepo)
     const user1 = new User('Jose', 'jose@mail.com', '1234')
-    const bikeId = await app.registerUser(user1)
+    await app.registerUser(user1)
     const bike = new Bike('caloi mountainbike', 'mountain bike',
         1234, 1234, 100.0, 'My bike', 5, [])
-    app.registerBike(bike)
+    const bikeId = await app.registerBike(bike)
     let persistedBike = await bikeRepo.find(bikeId)
-    console.log('Bike disponível: ', bike.available)
-    await app.rentBike(bike.id, user1.email)
+    console.log('Bike disponível: ', persistedBike.available)
+    await app.rentBike(bikeId, user1.email)
     persistedBike = await bikeRepo.find(bikeId)
-    console.log('Bike disponível: ', bike.available)
-    clock.tick(1000 * 60 * 65)
-    console.log(await app.returnBike(bike.id, user1.email))
-    persistedBike = await bikeRepo.find(bikeId)
-    console.log('Bike disponível: ', bike.available)
+    console.log('Bike disponível: ', persistedBike.available)
+    clock.tick(1000 * 60 * 60)
+    console.log(await app.returnBike(bikeId, user1.email))
+    console.log('Bike disponível: ', persistedBike.available)
 }
 
 main()
